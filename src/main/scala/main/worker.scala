e87db4279e0ab199a2d7c17c4beeeefc5526b6cf
songsm921 <songsm921@postech.ac.kr>
2022-12-07 05:32:39 +0900

    test

--- src/main/scala/main/worker.scala
+++ src/main/scala/main/worker.scala
@@ -1,11 +1,15 @@
 package main
 import utils.{Phase, util, workerPhase}
 import network.workerClient
+import network.MasterServer
+
 import java.io._
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext
 
 
 object worker {
+  val workerPort = 18219
   def main(args: Array[String]): Unit = {
     val parameter = util.parseArguments(args)
     val masterIP = parameter._1
@@ -21,6 +25,16 @@ object worker {
     }
     try{
       client.connect2Server()
+      val server = new MasterServer(ExecutionContext.global,client.totalWorkerNum-1,workerPort)
+      server.start()
+      server.printEndpoint()
+      for(i<-0 until client.totalWorkerNum){
+        if(i != client.myWorkerNum){
+          val worker2worker = new workerClient(client.workersIPList(i),workerPort,outputPath)
+          worker2worker.connect2Server()
+        }
+
+      }
     }
     catch {
       case e: Exception => println("Exception: " + e)
